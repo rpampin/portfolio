a1d3ec571029fa3c06a1febf44d3e80e2e923fef
Rodrigo Pampin <pampin.r@fateryh.org.ar>
2020-01-09 14:01:20 -0300

    changes

--- src/components/Home/Home.component.js
+++ src/components/Home/Home.component.js
@@ -1,13 +1,46 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Container from '@material-ui/core/Container'
 
-class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Home</h1>
-      </div>
-    )
-  }
+const Home = (props) => {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ height: "100%" }}
+      >
+        <Grid item xs={3}>
+          <Container className={classes.container}>
+            <h1>Home</h1>
+            <p>Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...</p>
+            <table>
+            <td><tr>ONE</tr></td>
+            <td><tr>TWO</tr></td>
+            <td><tr>THREE</tr></td>
+            <td><tr>FOUR</tr></td>
+            <td><tr>FIVE</tr></td>
+            </table>
+          </Container>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
 
-export default Home;
+const styles = theme => ({
+  root: {
+    backgroundColor: 'darkgrey'
+  },
+  container: {
+    backgroundColor: 'black'
+  }
+});
+
+export default withStyles(styles)(Home);
