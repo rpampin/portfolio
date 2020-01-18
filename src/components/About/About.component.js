e8d78229fa671c38020edbe04053508db3c2d383
Rodrigo <rodrigo.pampin@gmail.com>
2020-01-18 18:12:45 -0300

    added icons

--- src/components/About/About.component.js
+++ src/components/About/About.component.js
@@ -4,6 +4,7 @@ import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import a from '@material-ui/icons/Twi'
 
 class About extends React.Component {
   render() {
